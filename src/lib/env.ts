ced9f63a4302b01e7b6fd8d9e48d7151ee3db240
duncangarde <5747810+duncangarde@users.noreply.github.com>
2026-05-02 14:59:21 +0100

    Let it get the vercel url instead of configuring

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -10,6 +10,6 @@ if (!process.env.NEXT_PUBLIC_BASE_URL) {
 
 export const BASE_URL = dev
 	? 'http://localhost:3000'
-	: process.env.NEXT_PUBLIC_BASE_URL!
+	: `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
 
 export const BLOG_DIR = 'blog'
