d285601c1fed4642600f5726916d462a962b171c
duncangarde <5747810+duncangarde@users.noreply.github.com>
2026-05-02 15:03:35 +0100

    Fix Deploy issues with studio client

--- src/app/(studio)/admin/[[...tool]]/page.tsx
+++ src/app/(studio)/admin/[[...tool]]/page.tsx
@@ -1,7 +1,4 @@
-'use client'
-
-import { NextStudio } from 'next-sanity/studio'
-import config from '$/sanity.config'
+import StudioClient from './StudioClient'
 
 export const dynamic = 'force-dynamic'
 
@@ -10,5 +7,5 @@ export const maxDuration = 60 // sec
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
-	return <NextStudio config={config} />
+	return <StudioClient />
 }
