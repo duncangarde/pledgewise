1249679c48232fea950c5ffc715dd46c2aa197b4
duncangarde <5747810+duncangarde@users.noreply.github.com>
2026-05-02 14:59:46 +0100

    Fix SSR issue
    Server side rendering has no notion of $window

--- src/app/(studio)/admin/[[...tool]]/page.tsx
+++ src/app/(studio)/admin/[[...tool]]/page.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import { NextStudio } from 'next-sanity/studio'
 import config from '$/sanity.config'
 
-export const dynamic = 'force-static'
+export const dynamic = 'force-dynamic'
 
 export const maxDuration = 60 // sec
 
